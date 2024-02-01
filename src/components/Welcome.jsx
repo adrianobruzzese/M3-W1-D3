b3192720d3ca0b3551ffc91f62c97c4eb9f52d42
Adriano Bruzzese <146066663+adrianobruzzese@users.noreply.github.com>
2024-02-01 16:32:00 +0100

    added-comments-area

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,7 +2,7 @@ import { Alert } from 'react-bootstrap'
 
 const Welcome = () => (
   <Alert className="text-center">
-    <h1>Benvenuti in EpiBooks!</h1>
+    <h1>Welcome to EpiBooks!</h1>
   </Alert>
 )
 
